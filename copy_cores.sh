8592b81e5d801b5f0ee9ec9b18a19cefa6f990aa
Nick  Pawlowski <n.pawlowski16@imperial.ac.uk>
2017-09-10 11:43:18 +0100

    include gambatte and mgba

--- copy_cores.sh
+++ copy_cores.sh
@@ -3,4 +3,5 @@
 cp snes9x2010/*.so rle_python_interface/snes9x2010_libretro.so
 cp stella-libretro/*.so rle_python_interface/stella_libretro.so
 cp Genesis-Plus-GX/*.so rle_python_interface/genesis_plus_gx_libretro.so
-cp gambattle/*.so rle_python_interface/gambattle_libretro.so
+cp gambatte/*.so rle_python_interface/gambatte_libretro.so
+cp mgba/mgba_libretro.so rle_python_interface/mgba_libretro.so
